574975217cfbce3f07535547c280cfffbe07f748
angelh187 <angelh187@nycstudents.net>
2026-02-03 10:17:16 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,17 @@
-x = 6
+""" x = 6
 y = float(7)
-print(x,y)
+print(x,y) """
+
+""" 
+values = [1,2.23,5,7,2,30,15]
+print(values) """
+
+def discount(isMember, Age, isResident):
+    if('itsMember = True, or Age >=65, or Age <=12, or isResident = True'):
+        print('discount')
+    else:
+        print('No discount')
+
+
+
+discount(False, 18 , False)
