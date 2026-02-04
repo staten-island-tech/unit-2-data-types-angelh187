cf7c1b1af1c64c87e3afbdc7a7143fa213879789
angelh187 <angelh187@nycstudents.net>
2026-02-04 10:22:44 -0500

    Update data.py

--- data.py
+++ data.py
@@ -2,15 +2,30 @@
 y = float(7)
 print(x,y) """
 
-""" 
-values = [1,2.23,5,7,2,30,15]
-print(values) """
+bill = float (input('how much was the bill?'))
+tip = int(input('how much do you want to tip?'))
+def total(bill, service):
+    total = float(bill + tip)
 
-def discount(isMember, Age, isResident):
+
+
+total(bill + tip)
+
+
+
+
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i) """
+
+""" def discount(isMember, Age, isResident):
     if('itsMember = True, or Age >=65, or Age <=12, or isResident = True'):
         print('discount')
     else:
         print('No discount')
 
 
-discount(False, 18 , False)
+discount(False, 18 , False) """
+
