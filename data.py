4a0fda4be97861316567817b33feaddb7ca6f632
angelh187 <angelh187@nycstudents.net>
2026-02-05 10:17:36 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,24 +1,26 @@
 """ x = 6
 y = float(7)
 print(x,y) """
-
+""" 
 bill = float (input('how much was the bill?'))
 tip = int(input('how much do you want to tip?'))
-def total(bill, service):
+
+def total(bill , tip):
     total = float(bill + tip)
 
+total(bill + tip) """
 
+""" 
 
-total(bill + tip)
-
-
-
-
-
-""" values = [1,2.23,5,7,2,30,15]
+values = [1,2.23,5,7,2,30,15]
 print(values)
 for i in values:
-    print(i) """
+    print(i)
+print(values[1])
+print(values[2])
+ """
+
+
 
 """ def discount(isMember, Age, isResident):
     if('itsMember = True, or Age >=65, or Age <=12, or isResident = True'):
@@ -28,4 +30,14 @@ for i in values:
 
 
 discount(False, 18 , False) """
+""" 
+"test"
+["t","e","s","t"] """
 
+""" x = "this is a thing"
+y = x.split()
+z = y[0]
+print(y)
+print(z) """
+
+sentence = input("input a sentence")
