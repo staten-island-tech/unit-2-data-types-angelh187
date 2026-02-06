eddc1b15c58e3fef00075abfc6cedc375b3d64ef
angelh187 <angelh187@nycstudents.net>
2026-02-06 10:07:24 -0500

    2/6

--- data.py
+++ data.py
@@ -41,6 +41,25 @@ print(y)
 print(z) """
 
 """ sentence = input("input a sentence") """
+""" 
+sentence = input("input a sentence") """
 
-sentence = input("input a sentence")
+""" day_of_week = input("what day is it?")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect") """
+""" 
+x = "test"
+print(f"hello {x}") """
 
+""" 
+temp = 75
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold') """
+
+Number= input("What number is it?")
