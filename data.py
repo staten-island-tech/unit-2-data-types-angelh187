cdeb7ea5d800e96b125ddcd6d7ea7acbab2d0c2c
angelh187 <angelh187@nycstudents.net>
2026-02-06 10:18:08 -0500

    Update data.py

--- data.py
+++ data.py
@@ -62,4 +62,11 @@ elif temp == 68:
 else:
     print('cold') """
 
+
 Number= input("What number is it?")
+
+if Number % 2 == 0 :
+        print ("Even Number")
+else:
+        print("Odd Number")
+
